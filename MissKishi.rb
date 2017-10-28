39059f61e9da22232da803504aa08c8edd417db2
Katchan <no1fushi@live.jp>
2017-10-28 22:40:15 +0900

    Bug fixing in progress

--- MissKishi.rb
+++ MissKishi.rb
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 # Test用プログラム 壁にそって進む
 
@@ -19,7 +18,7 @@ count = 0
 dog = 0     # 斜めの位置に相手がいたときにカウントアップ
 ringo = 0   # dogの値が3になったらリンゴの値を１にする. リンゴの値が１の時、敵から離れたいのか近づくのか \ｺﾚｶﾞﾜｶﾗﾅｲ/
 red = 0     # stageとmodeを使い分けるためのカウンタ(2回ごとに切り替え)
-stage = 1   # 1Down, 2Right, 3Up, 4Left
+stage = 1 # 1Down, 2Right, 3Up, 4Left
 mode = 1    # 1Down, 2Left, 3Up, 4Right
 cat = 0     # 袋小路対策(同じ辺上の斜め2マスが壁で, 挟まれた1マスがアイテムの時, アイテムの奥が壁か否か確認する) 向きはmodeと同じ
 
@@ -43,8 +42,6 @@ def _moshiTonariNiTeki(values, action) # getReadyで隣接する敵を見つけ�
   end
 end
 
-
-
 def _moshiNanameNiTeki(values, dog, ringo) # getReadyで斜め位置に敵を見つけた時の処理
   if values[1] == 1 && ringo == 1
     mode = 3
@@ -65,33 +62,30 @@ def _moshiNanameNiTeki(values, dog, ringo) # getReadyで斜め位置に敵を見
   end
 end
 
-def _moshiItemGaFukurokozi(values, cat, mode, stage action) # getReadyで袋小路の入り口っぽいところにアイテムがある時
+def _moshiItemGaFukurokozi(values, cat, mode, stage, action) # getReadyで袋小路の入り口っぽいところにアイテムがある時
   if cat == 1 && values[8] = 2      # catが1で,values[8]が壁のとき
     cat = 0                       # catを０にする
     mode =1                       # modeを１にする
     stage = 1                     # stageを１にする
     # 袋小路に入らずに下に離脱する
-  end　# ここにendを書くと, 以降のelsifに分岐できません.(以下同様)
-elsif cat == 2 && values[6]= 2    #
+  　# ここにendを書くと, 以降のelsifに分岐できません.(以下同様)
+  elsif cat == 2 && values[6]= 2    #
   cat = 0                       #
   mode = 2                      #
   stage = 4                     #
   #袋小路に入らずに右に離脱する
-end
-elsif cat == 3 && values[2] = 2   #
+  elsif cat == 3 && values[2] = 2   #
   cat = 0                       #
   mode = 3                      #
   stage = 3                     #
   #袋小路に入らずに上に離脱する
-end
-elsif cat == 4 && values[4] = 2   #
+  elsif cat == 4 && values[4] = 2   #
   cat = 0                       #
   mode = 4                      #
   stage = 2                     #
   #袋小路に入らずに左に離脱する
-end　#条件分岐ここまで
-# end ←koreiru?
-# end ←これ要る？
+  end
+end
 
 def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
   if values[1] == 1 && ringo !=1        #
@@ -110,7 +104,9 @@ def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
     values=target.lookDown               #
     dog = dog +1
     action = 1                         #
-  elsif values[8]==3         #下にアイテムが有るなら
+  end
+
+  if values[8]==3         #下にアイテムが有るなら
     if values[7] == 2 && values[9] == 2 && cat !=5
       values=target.lookDown                       #
       mode = 1                                     #
@@ -161,8 +157,10 @@ def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
       cat = 0                                        #
       stage = 2
       action = 1
-    end                                              #
-  elsif mode == 1                                    #
+    end
+  end
+
+  if mode == 1                                    #
     if values[8] != 2           #下に壁がなければ下へ
       values = target.walkDown                         #
       mode = 1                                        #
@@ -192,7 +190,7 @@ def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
       stage = 4
       action = 1
     elsif values[8] != 2                              #
-      values=target.walkDown                          #
+      values = target.walkDown                          #
       mode = 1                                        #
       stage = 1
       action = 1
@@ -223,8 +221,8 @@ def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
       mode = 1                                        #
       stage =1
       action = 1
-    elsif   values[4] != 2                            #
-      values=target.walkLeft                          #
+    elsif values[4] != 2                            #
+      values = target.walkLeft                          #
       mode = 4                                        #
       stage = 2
       action = 1
@@ -241,12 +239,12 @@ def _redGaZeroOrOne(values, ringo, dog, cat, mode, stage, action)
       stage = 3
       action = 1
     elsif   values[6] != 2                            #
-      values=target.walkRight                         #
+      values = target.walkRight                         #
       mode = 2                                        #
       stage = 4
       action = 1
     elsif values[8] != 2                              #
-      values=target.walkDown                          #
+      values = target.walkDown                          #
       mode = 1                                        #
       stage = 1
       action = 1
@@ -270,8 +268,10 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
   elsif values[9] == 1 && ringo !=1     #
     values=target.lookDown               #
     dog = dog +1
-    action = 1                         #
-  elsif values[8]==3         #下にアイテムが有るなら
+    action = 1
+  end
+                         #
+  if values[8]==3         #下にアイテムが有るなら
     if values[7] == 2 && values[9] == 2 && cat !=5
       values=target.lookDown                       #
       mode = 1                                     #
@@ -322,8 +322,10 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       cat = 0                                        #
       stage = 2
       action = 1
-    end                                              #
-  elsif stage == 1                                    #
+    end
+  end
+
+  if stage == 1                                    #
     if values[8] != 2           #下に壁がなければ下へ
       values = target.walkDown                         #
       mode = 1                                        #
@@ -336,7 +338,7 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       stage = 2
       action = 1
     elsif values[2] != 2                              #
-      values=target.walkUp                            #
+      values = target.walkUp                            #
       mode = 3                                        #
       stage = 3
       action = 1
@@ -345,7 +347,7 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       mode = 2                                        #
       stage = 4
       action = 1
-    end                                               #
+    end                                              #
   elsif stage == 2                                     #
     if values[6] != 2                               #
       values = target.walkRight                       #
@@ -353,7 +355,7 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       stage = 2
       action = 1
     elsif values[8] != 2                              #
-      values=target.walkDown                          #
+      values = target.walkDown                          #
       mode = 1                                        #
       stage = 1
       action = 1
@@ -385,7 +387,7 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       stage = 1
       action = 1
     elsif   values[4] != 2                            #
-      values=target.walkRight                          #
+      values = target.walkRight                          #
       mode = 4                                        #
       stage = 2
       action = 1
@@ -401,13 +403,13 @@ def _redGaTwoOrThree(values, ringo, dog, cat, mode, stage)
       mode = 3                                        #
       stage = 3
       action = 1
-    elsif   values[6] != 2                            #
-      values=target.walkRight                         #
+    elsif values[6] != 2                            #
+      values = target.walkRight                         #
       mode = 4                                        #
       stage = 2
       action = 1
     elsif values[8] != 2                              #
-      values=target.walkDown                          #
+      values = target.walkDown                          #
       mode = 1                                        #
       stage = 1
       action = 1
@@ -437,11 +439,7 @@ def _changeMode(count, work, mode, stage)
       stage = work +3
     end                                                           #
   end
-
-
 end
-
-
 
 loop do # 無限ループ
   values = target.getReady # 準備信号を送り制御情報と周囲情報を取得
@@ -480,3 +478,4 @@ loop do # 無限ループ
 end
 
 target.close # ソケットを閉じる
+
