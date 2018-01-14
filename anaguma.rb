c39f42c67e82412881132b3ae0a2e7fa603f0080
PMjotaro <kaito.946nct@gmail.com>
2018-01-14 16:25:14 +0900

    Add files via upload

--- anaguma.rb
+++ anaguma.rb
@@ -2,40 +2,45 @@
 
 # 確殺
 
-def _anaguma target, seaval, values
+def _autoReady values
   values = target.getReady
+  _enemy values
+end
+
+def _anaguma target, seaval, values
+  _autoReady
   zone = 0
   if seaval[2] == 1 && seaval[4] == 1
     case $direction
     when 0 then
       values = target.walkRight
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookUp
         if seaval[4] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkUp
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putLeft
-      values = target.getReady
+      _autoReady
       values = target.putUp
     when 9 then
       values = target.walkDown
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookLeft
         if seaval[2] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkLeft
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putLeft
-      values = target.getReady
+      _autoReady
       values = target.putUp
     end
   elsif seaval[2] == 1 && seaval[6] == 1
@@ -43,32 +48,32 @@ def _anaguma target, seaval, values
     when 0 then
       values = target.walkLeft
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookUp
         if seaval[6] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkUp
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putRight
-      values = target.getReady
+      _autoReady
       values = target.putUp
     when 3 then
       values = target.walkDown
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookLeft
         if seaval[2] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkRight
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putRight
-      values = target.getReady
+      _autoReady
       values = target.putUp
     end
   elsif seaval[4] == 1 && seaval[8] == 1
@@ -76,32 +81,32 @@ def _anaguma target, seaval, values
     when 6 then
       values = target.walkLeft
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookDown
         if seaval[4] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkDown
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putLeft
-      values = target.getReady
+      _autoReady
       values = target.putDown
     when 9 then
       values = target.walkUp
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookLeft
         if seaval[8] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkLeft
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putLeft
-      values = target.getReady
+      _autoReady
       values = target.putDown
     end
 
@@ -110,32 +115,32 @@ def _anaguma target, seaval, values
     when 6 then
       values = target.walkLeft
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookDown
         if seaval[6] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkDown
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putRight
-      values = target.getReady
+      _autoReady
       values = target.putDown
     when 3 then
       values = target.walkUp
       while zone == 0 do
-        values = target.getReady
+        _autoReady
         seaval = target.lookRight
         if seaval[8] == 1
-          values = target.getReady
+          _autoReady
           values = target.walkRight
           zone = 1
         end
       end
-      values = target.getReady
+      _autoReady
       values = target.putRight
-      values = target.getReady
+      _autoReady
       values = target.putDown
     end
   end
