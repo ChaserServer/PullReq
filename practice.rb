23a3e4b6b751199bff95dd0d9d63ab9a2754a16a
PMjotaro <kaito.946nct@gmail.com>
2018-07-06 15:34:49 +0900

    明日の講習会最低限ここまで目指して

---
+++ practice.rb
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+require  'CHaserConnect.rb' #呼び出すおまじない
+
+# 書き換えない
+target = CHaserConnect.new("prac") # ()の中好きな名前
+values = Array.new(10)
+random = Random.new # 乱数生成
+
+hoko = 0 # 向きは時計の針の向き
+
+#壁を避けよう
+def _kabeyoke(hoko, values)
+	loop do
+		if hoko == 0 #上に行きたい
+			if values[2] == 2 #上のマスが壁だったら
+				hoko = 3 #右向け右
+			else
+				break
+			end
+		elsif hoko == 3 #右に行きたい
+			if values[6] == 2 #右のマスが壁だったら
+				hoko = 6 #下向け下
+			else
+				break
+			end
+		elsif hoko == 6 #下に行きたい
+			if values[8] == 2 #下のマスが壁だったら
+				hoko = 9 #左向け左
+			else
+				break
+			end
+		elsif hoko == 9 #左に行きたい
+			if values[4] == 2 #左のマスが壁だったら
+				hoko = 0 #上向け上
+			else
+				break
+			end
+		end
+	end
+	#puts hoko
+	return hoko
+end
+
+def _mayokonitekigairu(hoko, values, target)
+	ita = 0 #フラグ
+	#all you needs is kill
+	if values[2] == 1 #上に敵がいたとき
+		hoko = 0
+		ita  = 1 #フララ”が立った！
+	elsif values[6] == 1 #右に敵がいたとき
+		hoko = 3
+		ita  = 1 #フララ”が立った！
+	elsif values[8] == 1 #下に敵がいたとき
+		hoko = 6
+		ita  = 1 #フララ”が立った！
+	elsif values[4] == 1 #左に敵がいたとき
+		hoko = 9
+		ita  = 1 #フララ”が立った！
+	end
+
+	puts ita
+
+	if ita == 1
+		ita = 0 #立てたフラグは寝かせましょう
+		_oku(hoko, values, target)
+	end
+end
+
+
+#歩こう
+def _aruku(hoko, values, target)
+	#私は元気
+	case hoko
+	when 0
+		#上を向いて歩こう
+		values = target.walkUp()
+	when 3
+		#右を向いて歩こう
+		values = target.walkRight()
+	when 6
+		#下を向いて歩こう
+		values = target.walkDown()
+	when 9
+		#左を向いて歩こう
+		values = target.walkLeft()
+	end
+end
+
+def _oku(hoko, values, target)
+	#私は元気
+	case hoko
+	when 0
+		#上を向いて歩こう
+		values = target.putUp()
+	when 3
+		#右を向いて歩こう
+		values = target.putRight()
+	when 6
+		#下を向いて歩こう
+		values = target.putDown()
+	when 9
+		#左を向いて歩こう
+		values = target.putLeft()
+	end
+end
+
+def _mayokoniaitemugaaru(hoko, values)
+	if values[2] == 3
+		hoko = 0
+	elsif values[6] == 3
+		hoko = 3
+	elsif values[8] == 3
+		hoko = 6
+	elsif values[4] == 3
+		hoko = 9
+	end
+
+	return hoko
+end
+
+
+loop do
+	values = target.getReady()
+	#すぐに敵が隣接しているか確かめましょう
+	_mayokonitekigairu(hoko, values, target)
+
+	#右に曲がろう
+	#if hoko == 0 #上に行きたい
+	#	if values[2] == 2 #上のマスが壁だったら
+	#		hoko = 3 #右に曲がる
+	#	end
+	#end
+
+	#壁をよけよう
+	hoko = _mayokoniaitemugaaru(hoko, values)
+	hoko = _kabeyoke(hoko, values)
+	puts hoko
+=begin
+	loop do
+		if hoko == 0 #上に行きたい
+			if values[2] == 2 #上のマスが壁だったら
+				hoko = 3 #右向け右
+			else
+				break
+			end
+		elsif hoko == 3 #右に行きたい
+			if values[6] == 2 #右のマスが壁だったら
+				hoko = 6 #下向け下
+			else
+				break
+			end
+		elsif hoko == 6 #下に行きたい
+			if values[8] == 2 #下のマスが壁だったら
+				hoko = 9 #左向け左
+			else
+				break
+			end
+		elsif hoko == 9 #左に行きたい
+			if values[4] == 2 #左のマスが壁だったら
+				hoko = 0 #上向け上
+			else
+				break
+			end
+		end
+	end
+=end
+
+	#行動しよう
+	_aruku(hoko, values, target)
+=begin
+	#ステップ5でメソッド化
+	#行動しよう
+	case hoko
+	when 0
+		#上を向いて歩こう
+		values = target.walkUp()
+	when 3
+		#右を向いて歩こう
+		values = target.walkRight()
+	when 6
+		#下を向いて歩こう
+		values = target.walkDown()
+	when 9
+		#左を向いて歩こう
+		values = target.walkLeft()
+	end
+=end
+
+#--------ここから--------
+	if values[0] == 0
+		break #breakしたら負け
+	end
+end #ループここまで
+
+target.close
+#-----ここまで書き換えない-----
